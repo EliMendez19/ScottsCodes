94f7ccfe87a1303c20ccb508ffa034a683317298
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-05-03 19:25:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-const redeemBtn = document.getElementById('redeem-btn');
-const nameInput = document.getElementById('name');
-const codeInput = document.getElementById('code');
-const darkModeBtn = document.getElementById('dark-mode-btn');
-const lightModeBtn = document.getElementById('light-mode-btn');
-const body = document.querySelector('body');
+const validCodes = ['dont', 'eliisgoated', 'thisworkedbruh'];
 
-redeemBtn.addEventListener('click', redeemCode);
+const redeemButton = document.getElementById('redeem-button');
+const codeInput = document.getElementById('code-input');
 
-function redeemCode() {
-  const validCodes = ['code1', 'code2', 'code3'];
-  const userCode = codeInput.value;
-  const userName = nameInput.value;
+redeemButton.addEventListener('click', function() {
+  const userCode = codeInput.value.trim();
+
   if (validCodes.includes(userCode)) {
     const webhookURL = 'https://discord.com/api/webhooks/1103472689617838100/0QvS9NMwI57qHCzEQ7C9JZmn1ftE3x_HvAB3JrwOIYBq8cE8htVYjqfphmlskbU0t4RY'; // Replace with your own webhook URL
-    const message = {content: `${userName} has redeemed the code ${userCode}.`};
+    const message = {content: `A user has redeemed the code ${userCode}.`};
     fetch(webhookURL, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(message),
     });
     alert('Code redeemed successfully!');
+  } else {
+    alert('Invalid code.');
   }
+});
+
+const darkModeButton = document.getElementById('dark-mode-button');
+const lightModeButton = document.getElementById('light-mode-button');
+const body = document.querySelector('body');
+
+darkModeButton.addEventListener('click', function() {
+  body.classList.add('dark-mode');
+});
+
+lightModeButton.addEventListener('click', function() {
+  body.classList.remove('dark-mode');
+});
