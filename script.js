e59ed305e4d2d1edda6ad4c12fd0d0e4dcc6b77d
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-05-03 19:22:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// Define an array of valid codes
-const validCodes = ['code1', 'code2', 'code3'];
-
-// Get the HTML elements
-const codeInput = document.getElementById('code-input');
-const nameInput = document.getElementById('name-input');
 const redeemBtn = document.getElementById('redeem-btn');
-const lightModeBtn = document.getElementById('light-mode-btn');
+const nameInput = document.getElementById('name');
+const codeInput = document.getElementById('code');
 const darkModeBtn = document.getElementById('dark-mode-btn');
+const lightModeBtn = document.getElementById('light-mode-btn');
+const body = document.querySelector('body');
 
-// Add event listeners
 redeemBtn.addEventListener('click', redeemCode);
-lightModeBtn.addEventListener('click', toggleLightMode);
-darkModeBtn.addEventListener('click', toggleDarkMode);
 
 function redeemCode() {
-  // Get the user's code and name
+  const validCodes = ['code1', 'code2', 'code3'];
   const userCode = codeInput.value;
   const userName = nameInput.value;
-  
-  // Check if the user's code is valid
   if (validCodes.includes(userCode)) {
-    // If the code is valid, create a webhook bot and send a message to a Discord server
-    const webhookURL = 'https://discord.com/api/webhooks/1234567890/abcdefg'; // Replace with your own webhook URL
-    const message = {content: `${userName}
+    const webhookURL = 'https://discord.com/api/webhooks/1103472689617838100/0QvS9NMwI57qHCzEQ7C9JZmn1ftE3x_HvAB3JrwOIYBq8cE8htVYjqfphmlskbU0t4RY'; // Replace with your own webhook URL
+    const message = {content: `${userName} has redeemed the code ${userCode}.`};
+    fetch(webhookURL, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(message),
+    });
+    alert('Code redeemed successfully!');
+  }
