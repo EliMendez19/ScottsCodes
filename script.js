0736442e9a48bfe49e0311a3d8da25ee2b28b77a
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-05-03 21:23:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.body.style.backgroundColor = "#343434"; // Replace with desired color code
+
 // Define an array of valid codes
 const validCodes = ['eliisgoated', 'hazemisbetter', 'impoor'];
 
@@ -8,12 +9,17 @@ const codeField = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const statusMsg = document.getElementById('status');
 
+// Set maximum length for name and code fields
+nameField.maxLength = 25;
+codeField.maxLength = 12;
+
 // Add event listener to submit button
 submitBtn.addEventListener('click', () => {
-  const name = nameField.value;
-  const code = codeField.value;
-  // Check if the user's code is valid
-  if (validCodes.includes(code)) {
+  const name = nameField.value.trim();
+  const code = codeField.value.trim();
+  
+  // Check if the user's code is valid and code and name fields are not empty
+  if (validCodes.includes(code) && name !== '' && code !== '') {
     // If the code is valid, create a webhook bot and send a message to a Discord server
     const webhookURL = 'https://discord.com/api/webhooks/1103472689617838100/0QvS9NMwI57qHCzEQ7C9JZmn1ftE3x_HvAB3JrwOIYBq8cE8htVYjqfphmlskbU0t4RY'; // Replace with your own webhook URL
     const message = {content: `${name} has redeemed the code ${code}.`};
@@ -24,6 +30,6 @@ submitBtn.addEventListener('click', () => {
     });
     statusMsg.textContent = 'Code redeemed successfully!';
   } else {
-    statusMsg.textContent = 'Invalid code.';
+    statusMsg.textContent = 'Invalid code or empty name/code fields.';
   }
 });
