9c8f8ad7122f505730492d88ae8e6987c80a921d
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-05-03 19:13:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-const validCodes = ['testmeboy', 'loleli', 'subtoelimendez19'];
+// Define an array of valid codes
+const validCodes = ['testmeboy', 'subtoeli', 'thisisatest'];
 
-const redeemButton = document.getElementById('redeem-button');
+// Get the HTML elements
 const codeInput = document.getElementById('code-input');
+const nameInput = document.getElementById('name-input');
+const redeemBtn = document.getElementById('redeem-btn');
+const lightModeBtn = document.getElementById('light-mode-btn');
+const darkModeBtn = document.getElementById('dark-mode-btn');
 
-redeemButton.addEventListener('click', function() {
-  const userCode = codeInput.value.trim();
+// Add event listeners
+redeemBtn.addEventListener('click', redeemCode);
+lightModeBtn.addEventListener('click', toggleLightMode);
+darkModeBtn.addEventListener('click', toggleDarkMode);
 
+function redeemCode() {
+  // Get the user's code and name
+  const userCode = codeInput.value;
+  const userName = nameInput.value;
+  
+  // Check if the user's code is valid
   if (validCodes.includes(userCode)) {
-    const webhookURL = 'https://discord.com/api/webhooks/1103472689617838100/0QvS9NMwI57qHCzEQ7C9JZmn1ftE3x_HvAB3JrwOIYBq8cE8htVYjqfphmlskbU0t4RY'; // Replace with your own webhook URL
-    const message = {content: `A user has redeemed the code ${userCode}.`};
-    fetch(webhookURL, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(message),
-    });
-    alert('Code redeemed successfully!');
-  } else {
-    alert('Invalid code.');
-  }
-});
-
-const darkModeButton = document.getElementById('dark-mode-button');
-const lightModeButton = document.getElementById('light-mode-button');
-const body = document.querySelector('body');
-
-darkModeButton.addEventListener('click', function() {
-  body.classList.add('dark-mode');
-});
-
-lightModeButton.addEventListener('click', function() {
-  body.classList.remove('dark-mode');
-});
+    // If the code is valid, create a webhook bot and send a message to a Discord server
+    const webhookURL = 'https://discord.com/api/webhooks/1234567890/abcdefg'; // Replace with your own webhook URL
+    const message = {content: `${userName}
