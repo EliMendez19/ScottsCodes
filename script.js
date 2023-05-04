860454408959583413b974468c031b9d5837dcc5
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-05-03 19:28:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-const validCodes = ['dont', 'eliisgoated', 'thisworkedbruh'];
+// Define an array of valid codes
+const validCodes = ['eliisgoated', 'hazemisbetter', 'impoor'];
 
-const redeemButton = document.getElementById('redeem-button');
-const codeInput = document.getElementById('code-input');
+// Get form elements
+const nameField = document.getElementById('name');
+const codeField = document.getElementById('code');
+const submitBtn = document.getElementById('submit-btn');
+const statusMsg = document.getElementById('status');
 
-redeemButton.addEventListener('click', function() {
-  const userCode = codeInput.value.trim();
-
-  if (validCodes.includes(userCode)) {
+// Add event listener to submit button
+submitBtn.addEventListener('click', () => {
+  const name = nameField.value;
+  const code = codeField.value;
+  // Check if the user's code is valid
+  if (validCodes.includes(code)) {
+    // If the code is valid, create a webhook bot and send a message to a Discord server
     const webhookURL = 'https://discord.com/api/webhooks/1103472689617838100/0QvS9NMwI57qHCzEQ7C9JZmn1ftE3x_HvAB3JrwOIYBq8cE8htVYjqfphmlskbU0t4RY'; // Replace with your own webhook URL
-    const message = {content: `A user has redeemed the code ${userCode}.`};
+    const message = {content: `${name} has redeemed the code ${code}.`};
     fetch(webhookURL, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(message),
     });
-    alert('Code redeemed successfully!');
+    statusMsg.textContent = 'Code redeemed successfully!';
   } else {
-    alert('Invalid code.');
+    statusMsg.textContent = 'Invalid code.';
   }
-});
-
-const darkModeButton = document.getElementById('dark-mode-button');
-const lightModeButton = document.getElementById('light-mode-button');
-const body = document.querySelector('body');
-
-darkModeButton.addEventListener('click', function() {
-  body.classList.add('dark-mode');
-});
-
-lightModeButton.addEventListener('click', function() {
-  body.classList.remove('dark-mode');
 });
