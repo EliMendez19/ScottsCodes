b8d1342d96ee3647b9e8760cdc8aa989471ccba1
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-05-08 23:50:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ const codeField = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const statusMsg = document.getElementById('status');
 
-// Set maximum length for name and code fields
-nameField.maxLength = 25;
-codeField.maxLength = 20;
+// Define cooldown time in seconds
+const COOLDOWN_TIME = 15;
+
+// Create an object to store cooldowns for each user
+const cooldowns = {};
 
 // Add event listener to submit button
 submitBtn.addEventListener('click', () => {
@@ -20,15 +22,22 @@ submitBtn.addEventListener('click', () => {
   
   // Check if the user's code is valid and code and name fields are not empty
   if (validCodes.includes(code) && name !== '' && code !== '') {
-    // If the code is valid, create a webhook bot and send a message to a Discord server
-    const webhookURL = 'https://discord.com/api/webhooks/1103472689617838100/0QvS9NMwI57qHCzEQ7C9JZmn1ftE3x_HvAB3JrwOIYBq8cE8htVYjqfphmlskbU0t4RY'; // Replace with your own webhook URL
-    const message = {content: `${name} has just redeemed a code: ${code}.`};
-    fetch(webhookURL, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(message),
-    });
-    statusMsg.textContent = 'Code redeemed successfully!';
+    // Check if the user is on cooldown
+    if (cooldowns.hasOwnProperty(name) && (Date.now() - cooldowns[name]) < (COOLDOWN_TIME * 1000)) {
+      const remainingTime = Math.ceil(((COOLDOWN_TIME * 1000) - (Date.now() - cooldowns[name])) / 1000);
+      statusMsg.textContent = `You are on cooldown for ${remainingTime} seconds.`;
+    } else {
+      // If the code is valid and the user is not on cooldown, redeem the code and set a new cooldown for the user
+      const webhookURL = 'https://discord.com/api/webhooks/1103472689617838100/0QvS9NMwI57qHCzEQ7C9JZmn1ftE3x_HvAB3JrwOIYBq8cE8htVYjqfphmlskbU0t4RY'; // Replace with your own webhook URL
+      const message = {content: `${name} has just redeemed a code: ${code}.`};
+      fetch(webhookURL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(message),
+      });
+      statusMsg.textContent = 'Code redeemed successfully!';
+      cooldowns[name] = Date.now();
+    }
   } else {
     statusMsg.textContent = 'Invalid code or empty name/code fields.';
   }
